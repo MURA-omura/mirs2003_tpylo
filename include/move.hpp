ae9be22c6fb380496e5443dc934a4cc4236256bd
Saraokb <sara20011009@icloud.com>
2020-11-13 15:56:26 +0900

    move.hppに関数を追加しました。

--- include/move.hpp
+++ include/move.hpp
@@ -31,37 +31,38 @@ typedef enum{
  */
 class Move{
 public:
+    Move();
     /**
      * @brief 直進状態の関数(通常状態)
      * @details 直進する関数をmove.cppに渡す
      */
-     Move (int straight);
+     Move(int straight);
 
     /**
      * @brief 障害物回避の関数
      * @details 90度回転する関数をmove.cppに渡す
      */
-    Move (int turn_quarter);
+    Move(int turn_quarter);
 
     /**
      * @brief 左に曲がる関数
      * @details 左に曲がる関数をmove.cppに渡す
      */
-    Move (int curve_l);
+    Move(int curve_l);
 
     /**
      * @brief 右に曲がる関数
      * @details 右に曲がる関数をmove.cppに渡す
      */
-    Move (int curve_r);
-    
+    Move(int curve_r);
+
 
 
 private:
     // 超音波の値を格納する配列
     std::array<Uss, uss_t::USS_NUM> uss_array;
- 
-        /**
+
+    /**
      * @brief 直進する関数
      */
     int straight;
